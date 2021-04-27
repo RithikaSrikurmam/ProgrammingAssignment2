8ab0b95b115419a4043f48700c3f00e51ff3b1fe
RithikaSrikurmam <81615969+RithikaSrikurmam@users.noreply.github.com>
2021-04-27 19:01:11 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -7,8 +7,16 @@
 ## This function creates a special "matrix" object that can cache 
 ## its inverse.
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()) { ## define the argument with default mode of "matrix"
+       inv <- NULL                             ## initialize inv as NULL; will hold value of matrix inverse 
+       set <- function(y) {                    ## define the set function to assign new 
+                x <<- y                             ## value of matrix in parent environment
+                inv <<- NULL                        ## if there is a new matrix, reset inv to NULL
+            }
+       get <- function() x                     ## define the get function - returns value of the matrix argument
+       setinverse <- function(inverse) inv <<- inverse  ## assigns value of inv in parent environment
+       getinverse <- function() inv                     ## gets the value of inv 
+       list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)      
 }
 
 
